6b0b409f3e8c2ea609b0e7ec38f5b43baf1ca906
nerr-0 <nduguiskarioti@gmail.com>
2026-04-08 12:55:57 +0300

    links update

--- src/integrations/supabase/clients2.ts
+++ src/integrations/supabase/clients2.ts
@@ -1,7 +1,7 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js'
 
 // Replace with your Supabase project details
-const supabaseUrl: string = import.meta.env.VITE_SUPABASE_PROJECT_URL
+const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey: string = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 // Strongly typed Supabase client
